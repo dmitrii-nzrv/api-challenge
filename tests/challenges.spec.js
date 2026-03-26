5c6cf75dd7c88b604fba7e19c2590f09e20b8781
Dmitrii <dmitrii.nazarov7703@gmail.com>
2026-03-27 00:04:54 +0300

    finish tasks up to №5

--- tests/challenges.spec.js
+++ tests/challenges.spec.js
@@ -4,7 +4,10 @@ let token;
 const url = 'https://apichallenges.eviltester.com/';
 
 test.describe('Challenges', () => {
-  test.beforeEach(async ({ request }) => {
+  // для того, чтобы воркер был только 1 и токен не пересоздавался
+  test.describe.configure({ mode: 'serial' });
+
+  test.beforeAll(async ({ request }) => {
     let r = await request.post(`${url}challenger`);
     token = r.headers();
     console.log(`${url}gui/challenges/${token['x-challenger']}`);
@@ -16,7 +19,29 @@ test.describe('Challenges', () => {
         'X-CHALLENGER': token['x-challenger'],
       },
     });
+  });
 
-    expect(1).toBe(1);
+  test('№3', async ({ request }) => {
+    let r = await request.get(`${url}todos`, {
+      headers: {
+        'X-CHALLENGER': token['x-challenger'],
+      },
+    });
+  });
+
+  test('№4', async ({ request }) => {
+    let r = await request.get(`${url}todo`, {
+      headers: {
+        'X-CHALLENGER': token['x-challenger'],
+      },
+    });
+  });
+
+  test('№5', async ({ request }) => {
+    let r = await request.get(`${url}todos/1`, {
+      headers: {
+        'X-CHALLENGER': token['x-challenger'],
+      },
+    });
   });
 });
